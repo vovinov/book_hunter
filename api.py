1f3097f546147f19ae89b74e1f446c40559cae20
vovinov <v.v.ovinov@yandex.ru>
2024-12-09 22:12:01 +0300

    Add hunt_book function & get_price function

--- api.py
+++ api.py
@@ -1,6 +1,12 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium_stealth import stealth
+
+from bs4 import BeautifulSoup
+
+import requests
 
 browser = webdriver.Chrome()
 
@@ -13,14 +19,33 @@ stealth(browser,
         )
 
 
-url = 'https://www.ozon.ru'
-
-
-try:
+def hunt_book(isbn, url):
     browser.get(url=url)
-    time.sleep(10)
-except Exception as e:
-    print(e)
-finally:
-    browser.close()
-    browser.quit()
+    time.sleep(2)
+    input_text = browser.find_element(By.NAME, "text")
+    input_text.clear()
+    time.sleep(2)
+    input_text.send_keys(isbn)
+    input_text.send_keys(Keys.ENTER)
+
+    return browser.current_url + '&sorting=price'
+
+
+def get_price(site):
+
+    request = requests.get(site)
+    print(request)
+
+
+if __name__ == "__main__":
+    try:
+        current_site = hunt_book("978-5-04-186916-8", 'https://www.ozon.ru')
+        time.sleep(2)
+        get_price(current_site)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        browser.close()
+        browser.quit()
